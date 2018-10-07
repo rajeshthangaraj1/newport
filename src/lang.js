9544a8edf90d5089b6c8bb7cc38eaf292ba710d5
rajeshthangaraj1 <rajeshthangaraj1@gmail.com>
2018-10-07 10:38:41 +0400

    new port

---
+++ src/lang.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+/* Home page navigation items */
+    navigation: {
+        home: 'Home',
+        myaccount: 'My Account',
+        vieworders: 'View Orders',
+        viewrewards: 'View Rewards',
+	    about: 'About Us',
+        privacy: 'Privacy Policy',
+        contact: 'Contact Us'
+    },
+	
+ 
+ /*all page meta tags */
+ site_title: 'ECCS',
+ site_title_with_separator: ' | ECCS ',
+ home_meta_title:"Welcome to ECCS ",
+ prodcuts_meta_title:"Products Details",
+ scan_qr_code_page:"Book a Table",
+ signin_meta_title:"User Signin",
+ signup_meta_title : "user signup",
+ forgotpassword_meta_title : "forgot password",
+ checkout_meta_title : "Checkout",
+ ordersuccess_meta_title : "Order Success",
+ about_meta_title : "Aboutus",
+ privacy_meta_title : "Privacy Policy",
+ myaccount_meta_title : "My account",
+ contactus_meta_title : "Contact us",
+ orders_meta_title : "Orders",
+ cart_page_title : "Cart",
+     
+};
